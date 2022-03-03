4e6729e174e2d02b2823ac969c700b8fdd99369f
mcparra <mc.parra@outlook.es>
2022-03-03 20:20:57 +0100

    Tercer commit, javadoc desde Eclipse

--- deposito/src/cuentas/CCuenta.java
+++ deposito/src/cuentas/CCuenta.java
@@ -1,4 +1,15 @@
+
 package cuentas;
+/**
+ * La clase CCuenta permite realizar operaciones de ingreso y retirada, así
+ * como llevar un registro de los nombres de los clientes, cuentas, saldo
+ * y tipo de interés.
+ * 
+ * @author mcparra 
+ * @version 0.5 
+ * @since 07/02/2022 
+ * @see <a href="https://github.com/mcparra/ed_Tarea04/blob/master/deposito/src/cuentas/CCuenta.java">Enlace GitHub</a>
+ */
 public class CCuenta {
 
 	private String nombre;
@@ -9,6 +20,13 @@ public class CCuenta {
 	public CCuenta() {
 	}
 
+	/**
+	 * @param nombre
+	 * @param numero cuenta
+	 * @param saldo
+	 * @param tipo de interes
+	 *
+	 */
 	public CCuenta(String nom, String cue, double sal, double tipo) {
 		nombre = nom;
 		cuenta = cue;
@@ -19,6 +37,10 @@ public class CCuenta {
 		return nombre;
 	}
 
+	/**
+	 * @param nombre
+	 *
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
@@ -27,6 +49,10 @@ public class CCuenta {
 		return cuenta;
 	}
 
+	/**
+	 * @param numero cuenta
+	 *
+	 */
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
@@ -35,6 +61,10 @@ public class CCuenta {
 		return saldo;
 	}
 
+	/**
+	 * @param saldo
+	 *
+	 */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
@@ -43,6 +73,10 @@ public class CCuenta {
 		return tipoInteres;
 	}
 
+	/**
+	 * @param tipo interes
+	 *
+	 */
 	public void setTipoInteres(double tipoInteres) {
 		this.tipoInteres = tipoInteres;
 	}
@@ -50,13 +84,24 @@ public class CCuenta {
 	public double estado() {
 		return saldo;
 	}
-
+/**
+ * El método ingresar funciona solo con cantidades positivas,
+ * en caso contrario indicará que la cantidad es negativa.
+ * 
+ * @param cantidad
+ * @throws Exception
+ */
 	public void ingresar(double cantidad) throws Exception {
 		if (cantidad < 0)
 			throw new Exception("No se puede ingresar una cantidad negativa");
 		saldo = saldo + cantidad;
 	}
-
+/**
+ * El método retirar permite solo la retirada de cantidades positivas y con
+ * saldo suficiente para poder realizarlo.
+ * @param cantidad
+ * @throws Exception
+ */
 	public void retirar(double cantidad) throws Exception {
 		if (cantidad <= 0)
 			throw new Exception("No se puede retirar una cantidad negativa");
